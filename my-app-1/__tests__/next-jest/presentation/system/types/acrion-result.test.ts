1416d248eff661ab76fa505cb1066a5bbbad40ca
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2025-11-22 09:37:58 +0000

    202511221837

---
+++ my-app-1/__tests__/next-jest/presentation/system/types/acrion-result.test.ts
@@ -0,0 +1,32 @@
+import { ActionResult } from '@/presentation/(system)/types/action-result';
+import { User } from '@/presentation/users/min/modules/users-types';
+
+// 型の制約によるコンパイルエラーを確認するコード
+
+test('test1', () => {
+  const users: User[] = [{ userId: '1234', userName: 'test taro' }];
+
+  // const _result1: ActionResult<User[]> = { abort: true, data: users }; // 型の制約でコンパイルエラーになる
+  const _result2: ActionResult<User[]> = { abort: true };
+  const _result3: ActionResult<User[]> = { abort: false, data: users };
+  // const result4: ActionResult<User[]> = { abort: false }; // 型の制約でコンパイルエラーになる
+});
+
+test('test2', () => {
+  //const _result1: ActionResult<void> = { abort: true, data: undefined }; // 型の制約でコンパイルエラーになる
+  const _result2: ActionResult<void> = { abort: true };
+  const _result3: ActionResult<void> = { abort: false, data: undefined };
+  //const _result4: ActionResult<void> = { abort: false }; // 型の制約でコンパイルエラーになる
+});
+
+test('test3', () => {
+  const users: User[] = [{ userId: '1234', userName: 'test taro' }];
+
+  // ファクトリ経由の生成
+  const _result1: ActionResult<User[]> = ActionResult.complete(users);
+  const _result2: ActionResult<User[]> = ActionResult.abort();
+  const _result3: ActionResult<void> = ActionResult.complete(undefined);
+  const _result4: ActionResult<void> = ActionResult.abort();
+  const _result5: ActionResult<undefined> = ActionResult.complete(undefined);
+  const _result6: ActionResult<undefined> = ActionResult.abort();
+});
