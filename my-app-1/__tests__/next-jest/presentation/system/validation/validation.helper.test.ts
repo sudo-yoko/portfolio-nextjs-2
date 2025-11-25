9ecafec86e5d88aad7865c189019748e05493bf8
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2025-11-25 14:54:04 +0000

    202511252354

---
+++ my-app-1/__tests__/next-jest/presentation/system/validation/validation.helper.test.ts
@@ -0,0 +1,24 @@
+import { isViolations } from '@/presentation/(system)/validation/validation.helper';
+
+// npm exec -- cross-env NODE_OPTIONS=--experimental-vm-modules jest __tests__/next-jest/presentation/system/validation/validation.helper.test.ts -t 'test1-1'
+test('test1-1', () => {
+  const text = ''; // JSONパース不可
+  const result = isViolations(text);
+  expect(result).toBe(false);
+});
+
+// npm exec -- cross-env NODE_OPTIONS=--experimental-vm-modules jest __tests__/next-jest/presentation/system/validation/validation.helper.test.ts -t 'test1-2'
+test('test1-2', () => {
+  const text = { name: [''], email: [''], body: [''] };
+  const json = JSON.stringify(text);
+  const result = isViolations(json);
+  expect(result).toBe(true);
+});
+
+// npm exec -- cross-env NODE_OPTIONS=--experimental-vm-modules jest __tests__/next-jest/presentation/system/validation/validation.helper.test.ts -t 'test2-1'
+test('test2-1', () => {
+  const text = { name: [''], email: [''], body: [''] };
+  const json = JSON.stringify(text);
+  const result = isViolations(json, 'name', 'email');
+  expect(result).toBe(false);
+});
