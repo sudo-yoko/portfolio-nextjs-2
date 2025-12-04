c9a4a03b23884a577e6f180ec9729b43ae1b3162
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2025-12-04 09:55:12 +0000

    202512041855

--- my-app-1/src/presentation/(system)/errors/views/component.error-redirect.tsx
+++ my-app-1/src/presentation/(system)/errors/views/component.error-redirect.tsx
@@ -4,11 +4,17 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export function ErrorRedirect() {
+  const redirected = useRef(false); // 再レンダリングなしで保持したい値を参照するためのフック
   const router = useRouter();
+
   useEffect(() => {
+    // devモードでは、StrictModeでuseEffectが２回実行されるためページ遷移が上手くいかないのでuseRefフラグで制御する
+    if (redirected.current) return;
+    redirected.current = true;
+
     router.replace('/system-error');
   }, [router]);
   return null;
