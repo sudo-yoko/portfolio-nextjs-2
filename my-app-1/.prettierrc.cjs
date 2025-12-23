a3a51c3769caaf076488691e79a0c1d2da751194
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2025-12-23 02:40:54 +0000

    202512231140

--- my-app-1/.prettierrc.cjs
+++ my-app-1/.prettierrc.cjs
@@ -1,14 +1,19 @@
 module.exports = {
-  // 1行の最大文字数（長すぎる行を自動で改行）
-  printWidth: 110,
+    // 1行の最大文字数（長すぎる行を自動で改行）
+    printWidth: 110,
 
-  // 文字列リテラルをシングルクォートに統一
-  singleQuote: true,
+    // 文字列リテラルをシングルクォートに統一
+    singleQuote: true,
 
-  // JSXではダブルクォートを使う（例: <div className="...">）
-  jsxSingleQuote: false,
+    // JSXではダブルクォートを使う（例: <div className="...">）
+    jsxSingleQuote: false,
 
-  // Tailwind CSS のクラス順を自動で整形
-  plugins: ['prettier-plugin-tailwindcss'],
+    // スペースでインデントする
+    useTabs: false,
+
+    // インデントのサイズ（スペースの数）
+    tabWidth: 4,
+
+    // Tailwind CSS のクラス順を自動で整形
+    plugins: ['prettier-plugin-tailwindcss'],
 };
-
---
+++ my-app-1/_docs/tips.md
@@ -0,0 +1,9 @@
+Prettier
+
+コード整形すると、コンソールの「出力」タブに.prettierrc.cjsの内容が出力される。
+.prettierrc.cjsを修正しても反映されなていない時は、codespaceをいったん切断し、再接続する
+
+setting.json
+  "editor.detectIndentation": false,
+  "editor.tabSize": 4
+→エディターで、スペース４つごとの縦線が入るようになる。.prettierrc.cjsのtabWidthも４にするとインデントと縦線が一致して見やすくなる
