964e50fed90fef165912d7869343694aae48fa8e
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2026-03-11 15:07:21 +0000

    202603120007

--- my-app-1/src/presentation/_system/client/client.c.ts
+++ my-app-1/src/presentation/_system/client/client.c.ts
@@ -1,11 +1,11 @@
+//
+// クライアントサイド専用 Client
+//
 import 'client-only';
 
 import { clientImpl } from '@/presentation/_system/client/client.impl.fetch';
 import { Client } from '@/presentation/_system/client/client.types';
 
-/**
- * BFF 呼び出しクライアント
- */
 const client: Client = clientImpl;
 
 export default client;
--- my-app-1/src/presentation/_system/client/client.s.ts
+++ my-app-1/src/presentation/_system/client/client.s.ts
@@ -1,11 +1,11 @@
+//
+// サーバーサイド専用 Client
+//
 import 'server-only';
 
 import { clientImpl } from '@/presentation/_system/client/client.impl.axios';
 import { Client } from '@/presentation/_system/client/client.types';
 
-/**
- * バックエンド呼び出しクライアント
- */
 const client: Client = clientImpl;
 
 export default client;
