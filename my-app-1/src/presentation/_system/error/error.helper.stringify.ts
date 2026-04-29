6059d30f578f87fb3900c7f2bdc665a1051cdcf4
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2026-04-29 08:59:05 +0000

    202604291758

--- my-app-1/src/presentation/_system/error/error.helper.stringify.ts
+++ my-app-1/src/presentation/_system/error/error.helper.stringify.ts
@@ -2,38 +2,45 @@ import axios from 'axios';
 
 /**
  * Errorオブジェクトの文字列表現を作成する
+ *
+ * @param error - キャッチされたエラー
+ * @param description - エラーの説明
+ * @param details - エラーの詳細
+ * @returns
+ * - `message`: エラーメッセージのみ（スタックトレースを含まない）
+ * - `all`: スタックトレースを含むすべてのメッセージ
  */
-export function stringify(
-    error: unknown,
-    description?: string,
-): {
-    /**
-     * エラーメッセージのみ（スタックトレースを含まない）
-     */
+export function stringify({
+    error,
+    description,
+    details,
+}: {
+    error?: unknown;
+    description?: string;
+    details?: string;
+}): {
     message: string;
-    /**
-     * スタックトレースを含むすべてのメッセージ
-     */
     all: string;
 } {
     if (typeof error === 'string') {
         const message = error;
-        return { message, all: joinAll({ description, message }) };
-    } else if (axios.isAxiosError(error)) {
-        const { message } = error;
-        const stacks = getStackTrace(error);
-        const all: All = { message, stacks };
-        if (error.response) {
-            all.details = `Axios Error: Response(Inbound) -> status=${error.response.status}, data=${error.response.data}`;
-        }
-        return { message, all: joinAll(all) };
+        return { message, all: joinAll({ message, description, details }) };
+        // } else if (axios.isAxiosError(error)) {
+        //     //TODO; axios固有のエラーはclient.adapterで処理できないか
+        //     const { message } = error;
+        //     const stacks = getStackTrace(error);
+        //     const all: All = { message, stacks };
+        //     if (error.response) {
+        //         all.details = `Axios Error: Response(Inbound) -> status=${error.response.status}, data=${error.response.data}`;
+        //     }
+        //     return { message, all: joinAll(all) };
     } else if (error instanceof Error) {
         const { message } = error;
         const stacks = getStackTrace(error);
-        return { message, all: joinAll({ description, message, stacks }) };
+        return { message, all: joinAll({ message, description, details, stacks }) };
     } else {
         const message = 'unknown type error.';
-        return { message, all: joinAll({ description, message }) };
+        return { message, all: joinAll({ message, description, details }) };
     }
 }
 
@@ -81,7 +88,7 @@ function joinAll({
 }): string {
     const errMessage: string[] = [];
     if (description) {
-        errMessage.push(description);
+        errMessage.push(`\ndescription: ${description}`);
     }
     if (message) {
         errMessage.push(`\nmessage: ${message}`);
@@ -97,3 +104,26 @@ function joinAll({
 
 // 引数の型
 type All = Parameters<typeof joinAll>[0];
+
+/**
+ * Errorオブジェクトの中からaxios固有のエラーを取得する
+ */
+export function getAxiosErrorProperties(err: unknown): string {
+    const description: Record<string, unknown> = {};
+    if (axios.isAxiosError(err)) {
+        description['code'] = err.code;
+        return JSON.stringify(description, null, 2);
+    }
+    return JSON.stringify(description, null, 2);
+}
+
+/**
+ * Errorオブジェクトの中からnode:http固有のエラーを取得する
+ */
+export function getNodeErrorProperties(err: Error): string {
+    const description: Record<string, unknown> = {};
+    if ('code' in err) {
+        description['code'] = err.code;
+    }
+    return JSON.stringify(description, null, 2);
+}
