8bbef9669468da78854b00a75a811552997534d9
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2026-04-30 19:46:09 +0000

    202605010440

--- my-app-1/src/app/healthcheck/page.tsx
+++ my-app-1/src/app/healthcheck/page.tsx
@@ -1,1 +1,5 @@
 export { default } from '@/presentation/healthcheck/views/healthcheck.page';
+
+// 動的レンダリングに強制変更する
+// productionビルドで、ServerCompoenetのサーバーサイドhttpリクエストが実行されてしまうため。
+export const dynamic = 'force-dynamic';
