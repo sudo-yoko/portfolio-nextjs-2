bb444d7ae3cec0a6a9cd4db85ae7ed051ee556ad
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2026-07-15 12:41:30 +0000

    202607152141

--- my-app-1/__tests__/node-test/presentation/system/search-params.test.ts
+++ my-app-1/__tests__/node-test/presentation/system/search-params.test.ts
@@ -1,16 +1,19 @@
 import test from 'node:test';
 
 import { printf } from '@/__tests__/test-logger';
-import { getStringParam, SearchParams } from '@/presentation/_system/types/search-params';
+import { getArrayParam, getStringParam, SearchParams } from '@/presentation/_system/types/search-params';
 
-const print = printf({ logPrefix: '>>> [search-params.test.ts]', stdout: true });
+const print = printf({ logPrefix: '>>> [search-params.test.ts]', stdout: false });
 
 // npm exec -- node --test --import tsx --test-name-pattern='^test1-1$' __tests__/node-test/presentation/system/search-params.test.ts
 test('test1-1', async (t) => {
     const logPrefix = `${t.name}: `;
 
-    const result = await getStringParam(undefined, 'key1');
-    print(logPrefix + `result=|${result}|`);
+    const str = await getStringParam(undefined, 'key1');
+    print(logPrefix + `result -> str=|${str}|`);
+
+    const arr = await getArrayParam(undefined, 'key1');
+    print(logPrefix + 'result -> arr=', arr);
 });
 
 // npm exec -- node --test --import tsx --test-name-pattern='^test1-2$' __tests__/node-test/presentation/system/search-params.test.ts
@@ -20,8 +23,11 @@ test('test1-2', async (t) => {
     const searchParams: SearchParams = Promise.resolve({
         key1: 'key1',
     });
-    const result = await getStringParam(searchParams, 'key0');
-    print(logPrefix + `result=|${result}|`);
+    const str = await getStringParam(searchParams, 'key0');
+    print(logPrefix + `result -> str=|${str}|`);
+
+    const arr = await getArrayParam(searchParams, 'key0');
+    print(logPrefix + 'result -> arr=', arr);
 });
 
 // npm exec -- node --test --import tsx --test-name-pattern='^test1-3$' __tests__/node-test/presentation/system/search-params.test.ts
@@ -31,8 +37,11 @@ test('test1-3', async (t) => {
     const searchParams: SearchParams = Promise.resolve({
         key1: '',
     });
-    const result = await getStringParam(searchParams, 'key1');
-    print(logPrefix + `result=|${result}|`);
+    const str = await getStringParam(searchParams, 'key1');
+    print(logPrefix + `result -> str=|${str}|`);
+
+    const arr = await getArrayParam(searchParams, 'key1');
+    print(logPrefix + 'result -> arr=', arr);
 });
 
 // npm exec -- node --test --import tsx --test-name-pattern='^test1-4$' __tests__/node-test/presentation/system/search-params.test.ts
@@ -42,8 +51,11 @@ test('test1-4', async (t) => {
     const searchParams: SearchParams = Promise.resolve({
         key1: undefined,
     });
-    const result = await getStringParam(searchParams, 'key1');
-    print(logPrefix + `result=|${result}|`);
+    const str = await getStringParam(searchParams, 'key1');
+    print(logPrefix + `result -> str=|${str}|`);
+
+    const arr = await getArrayParam(searchParams, 'key1');
+    print(logPrefix + 'result -> arr=', arr);
 });
 
 // npm exec -- node --test --import tsx --test-name-pattern='^test1-5$' __tests__/node-test/presentation/system/search-params.test.ts
@@ -53,8 +65,11 @@ test('test1-5', async (t) => {
     const searchParams: SearchParams = Promise.resolve({
         key1: '  　',
     });
-    const result = await getStringParam(searchParams, 'key1');
-    print(logPrefix + `result=|${result}|`);
+    const str = await getStringParam(searchParams, 'key1');
+    print(logPrefix + `result -> str=|${str}|`);
+
+    const arr = await getArrayParam(searchParams, 'key1');
+    print(logPrefix + 'result -> arr=', arr);
 });
 
 // npm exec -- node --test --import tsx --test-name-pattern='^test2-1$' __tests__/node-test/presentation/system/search-params.test.ts
@@ -64,8 +79,11 @@ test('test2-1', async (t) => {
     const searchParams: SearchParams = Promise.resolve({
         key1: ['', 'key1'],
     });
-    const result = await getStringParam(searchParams, 'key1');
-    print(logPrefix + `result=|${result}|`);
+    const str = await getStringParam(searchParams, 'key1');
+    print(logPrefix + `result -> str=|${str}|`);
+
+    const arr = await getArrayParam(searchParams, 'key1');
+    print(logPrefix + 'result -> arr=', arr);
 });
 
 // npm exec -- node --test --import tsx --test-name-pattern='^test2-2$' __tests__/node-test/presentation/system/search-params.test.ts
@@ -75,8 +93,11 @@ test('test2-2', async (t) => {
     const searchParams: SearchParams = Promise.resolve({
         key1: ['  　', 'key1'],
     });
-    const result = await getStringParam(searchParams, 'key1');
-    print(logPrefix + `result=|${result}|`);
+    const str = await getStringParam(searchParams, 'key1');
+    print(logPrefix + `result -> str=|${str}|`);
+
+    const arr = await getArrayParam(searchParams, 'key1');
+    print(logPrefix + 'result -> arr=', arr);
 });
 
 // npm exec -- node --test --import tsx --test-name-pattern='^test2-3$' __tests__/node-test/presentation/system/search-params.test.ts
@@ -86,7 +107,30 @@ test('test2-3', async (t) => {
     const searchParams: SearchParams = Promise.resolve({
         key1: [' key10 　', 'key1'],
     });
-    const result = await getStringParam(searchParams, 'key1');
-    print(logPrefix + `result=|${result}|`);
+    const str = await getStringParam(searchParams, 'key1');
+    print(logPrefix + `result -> str=|${str}|`);
+
+    const arr = await getArrayParam(searchParams, 'key1');
+    print(logPrefix + 'result -> arr=', arr);
 });
 
+// npm exec -- node --test --import tsx --test-name-pattern='^test2-4$' __tests__/node-test/presentation/system/search-params.test.ts
+test('test2-4', async (t) => {
+    const logPrefix = `${t.name}: `;
+
+    const searchParams: SearchParams = Promise.resolve({
+        key1: ['key11', 'key12'],
+        key2: 'key2',
+    });
+    const str = await getStringParam(searchParams, 'key1');
+    print(logPrefix + `result -> str=|${str}|`);
+
+    const arr = await getArrayParam(searchParams, 'key1');
+    print(logPrefix + 'result -> arr=', arr);
+
+    const str2 = await getStringParam(searchParams, 'key2');
+    print(logPrefix + `result -> str=|${str2}|`);
+
+    const arr2 = await getArrayParam(searchParams, 'key2');
+    print(logPrefix + 'result -> arr=', arr2);
+});
--- my-app-1/src/presentation/_system/types/search-params.ts
+++ my-app-1/src/presentation/_system/types/search-params.ts
@@ -12,16 +12,15 @@ export async function getStringParam(searchParams: SearchParams | undefined, key
     const params = await searchParams;
     const param = params?.[key];
     const value = Array.isArray(param) ? param[0] : param;
-    return value ? value.trim() : '';
-    // if (!searchParams) {
-    //     return '';
-    // }
+    return value ? value : '';
+}
+
+export async function getArrayParam(searchParams: SearchParams | undefined, key: string): Promise<string[]> {
+    const params = await searchParams;
+    const value = params?.[key];
+    if (Array.isArray(value)) {
+        return value;
+    }
     // NOTE: 空文字もFalsyとなる
-    // if (!param) {
-    //     return '';
-    // }
-    // if (Array.isArray(param)) {
-    //     return param[0];
-    // }
-    // return param.trim();
+    return value ? [value] : [];
 }
