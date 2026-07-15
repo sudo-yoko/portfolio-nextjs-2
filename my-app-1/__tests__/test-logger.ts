98e0b9bb81e07d86094e29ba0eac11f5cac504a2
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2026-07-15 12:41:51 +0000

    202607152141

--- my-app-1/__tests__/test-logger.ts
+++ my-app-1/__tests__/test-logger.ts
@@ -5,8 +5,8 @@
  * useStdoutにtrueを設定することで、直接process.stdout.writeでログ出力できる関数。
  */
 export function printf({ logPrefix, stdout }: { logPrefix: string; stdout: boolean }) {
-  return (...s: unknown[]) => {
-    const message = [logPrefix, ...s];
-    return stdout ? process.stdout.write(message.join(' ') + '\n') : console.log(message);
-  };
+    return (...s: unknown[]) => {
+        const message = [logPrefix, ...s];
+        return stdout ? process.stdout.write(message.join(' ') + '\n') : console.log(...message);
+    };
 }
