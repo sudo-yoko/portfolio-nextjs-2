b37dbf00e12fc08cfa7566978a7e28cbda99093b
Yoko, Sudo <83765620+sudo-yoko@users.noreply.github.com>
2026-07-23 16:34:19 +0000

    202607240134

--- my-app-1/__tests__/next-jest/presentation/system/result/result.bff.factories.test.ts
+++ my-app-1/__tests__/next-jest/presentation/system/result/result.bff.factories.test.ts
@@ -1,17 +1,17 @@
-import { ok } from '../../../../_proto/result.bff.factories';
+// import { ok } from '../../../../../__development/_bk/_proto/result.bff.factories';
 
-test('test1-1', () => {
-  const result = ok();
-});
+// test('test1-1', () => {
+//   const result = ok();
+// });
 
-test('test1-2', () => {
-  const data = {
-    userName: 'test taro',
-  };
-  const result = ok(data);
-});
+// test('test1-2', () => {
+//   const data = {
+//     userName: 'test taro',
+//   };
+//   const result = ok(data);
+// });
 
-test('test1-3', () => {
-  // TODO: dataにnullも入れられる
-  const result = ok(null);
-});
+// test('test1-3', () => {
+//   // TODO: dataにnullも入れられる
+//   const result = ok(null);
+// });
